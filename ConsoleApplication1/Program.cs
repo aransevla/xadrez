a27a0984072100c023dff9efb5052f60fdb3f538
Nara Caroline <zumiizombie@gmail.com>
2017-11-29 10:27:53 -0200

    Projeto iniciado

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+using System.Globalization;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
