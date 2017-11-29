0fe679642506cc8f6e899ec81030a0dd0e0ea4fa
Nara Caroline <zumiizombie@gmail.com>
2017-11-29 11:05:19 -0200

    Comecando a implementar o tabuleiro

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,14 +10,8 @@ namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P;
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
-
-
-
 
         }
     }
